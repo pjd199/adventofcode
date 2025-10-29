5e2e4fc65337095b5ae6fa4a9f4dc790d36b0fb1
pjd199 <89210268+pjd199@users.noreply.github.com>
2025-10-29 14:43:20 +0000

    solved puzzle

---
+++ src/adventofcode/year2015/day08.py
@@ -0,0 +1,31 @@
+"""Advent of Code 2015.
+
+Day 8: Matchsticks
+
+https://adventofcode.com/2015/day/8
+"""
+
+from re import findall
+
+from adventus import Puzzle
+
+
+def solve() -> None:
+    """Solve the puzzle."""
+    # initiate the puzzle
+    puzzle = Puzzle(8, 2015)
+
+    # solve part one
+    puzzle.answer_one = sum(
+        len(x) - len(findall(r"\\x[0-9a-f]{2}|\\\"|\\\\|.", x[1:-1]))
+        for x in puzzle.input.splitlines()
+    )
+
+    # solve part two
+    puzzle.answer_two = sum(
+        len(findall(r"\\|\"", x)) + 2 for x in puzzle.input.splitlines()
+    )
+
+
+if __name__ == "__main__":
+    solve()
