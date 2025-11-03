7acaa971ad2f3357a491f81b67c07e6f04d3d788
pjd199 <89210268+pjd199@users.noreply.github.com>
2025-11-03 15:56:04 +0000

    puzzle solved

---
+++ src/adventofcode/year2015/day12.py
@@ -0,0 +1,38 @@
+"""Advent of Code 2015.
+
+Day 12: JSAbacusFramework.io
+
+https://adventofcode.com/2015/day/12
+"""
+
+from json import loads
+from re import findall
+from typing import Any
+
+from adventus import Puzzle
+
+
+def count(obj: Any) -> int:
+    if isinstance(obj, int):
+        return int(obj)
+    if isinstance(obj, list):
+        return sum(count(x) for x in obj)
+    if isinstance(obj, dict) and "red" not in obj.values():
+        return sum(count(x) for x in obj.values())
+    return 0
+
+
+def solve() -> None:
+    """Solve the puzzle."""
+    # initiate the puzzle
+    puzzle = Puzzle(12, 2015)
+
+    # solve part one
+    puzzle.answer_one = sum(int(x) for x in findall(r"-?\d+", puzzle.input))
+
+    # solve part two
+    puzzle.answer_two = count(loads(puzzle.input))
+
+
+if __name__ == "__main__":
+    solve()
