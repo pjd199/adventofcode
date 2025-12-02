32366a4c9f8d2ab9ab508d7761c2d6c0bb4ed0e6
pjd199 <89210268+pjd199@users.noreply.github.com>
2025-12-02 21:29:05 +0000

    solved puzzle

---
+++ src/adventofcode/year2025/day02.py
@@ -0,0 +1,37 @@
+"""Advent of Code 2025.
+
+Day 2: Gift Shop
+
+https://adventofcode.com/2025/day/2
+"""
+
+from re import findall, fullmatch
+
+from adventus import Puzzle
+
+
+def solve() -> None:
+    """Solve the puzzle."""
+    # initiate the puzzle
+    puzzle = Puzzle(2, 2025)
+    data = puzzle.input
+
+    # solve part one
+    puzzle.answer_one = sum(
+        number
+        for first, last in findall(r"(\d+)-(\d+)", data)
+        for number in range(int(first), int(last) + 1)
+        if fullmatch(r"(\d+)\1", str(number))
+    )
+
+    # solve part two
+    puzzle.answer_two = sum(
+        number
+        for first, last in findall(r"(\d+)-(\d+)", data)
+        for number in range(int(first), int(last) + 1)
+        if fullmatch(r"(\d+)\1+", str(number))
+    )
+
+
+if __name__ == "__main__":
+    solve()
